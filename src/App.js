04b73e94a7ef2bed4f5ac9bb4fa124c6c7b21fb9
Patricio Soriano - SIGdeletras <pasoriano@gmail.com>
2020-08-16 14:07:57 +0200

    pasado useeffect a componente

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 /* eslint-disable react/jsx-one-expression-per-line */
-import React, { useState, useEffect } from 'react';
-import getGeocoders from './services/getCartociudadCandidates';
+import React from 'react';
+
 import './App.css';
 
 import ListOfSingleGeocode from './components/ListOfSingleGeocode';
 
 function App() {
-  const [geocoders, setGeocoders] = useState([]);
-
-  useEffect(() => {
-    getGeocoders({ direccion: 'plaza corredera 2 córdoba' })
-      .then((geocoders) => setGeocoders(geocoders));
-  }, []);
-
   return (
     <div className="App">
       <p>Hola</p>
       <ul className="listing">
-        <ListOfSingleGeocode geocoders={geocoders} />
+        <ListOfSingleGeocode direccion="Plaza de la Corredera" />
 
       </ul>
 
--- src/components/ListOfSingleGeocode/index.js
+++ src/components/ListOfSingleGeocode/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import getGeocoders from '../../services/getCartociudadCandidates';
 import SingleGeocode from '../SingleGeocode';
 
-export default function ListOfSingleGeocode({ geocoders }) {
+export default function ListOfSingleGeocode({ direccion }) {
+  const [geocoders, setGeocoders] = useState([]);
+
+  useEffect(() => {
+    getGeocoders({ direccion })
+      .then((geocoders) => setGeocoders(geocoders));
+  }, []);
+
   return (
 
     geocoders.map((singleGeocode) => (
