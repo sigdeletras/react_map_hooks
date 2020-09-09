1c8f63ee1706f0fb691c55173f864134e24d4883
Patricio Soriano - SIGdeletras <pasoriano@gmail.com>
2020-09-09 16:07:40 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React, { useState } from 'react';
+import { Router, Route, browserHistory } from 'react-router';
 
 import './App.css';
 
@@ -9,13 +10,17 @@ function App() {
   const [direccion, setDireccion] = useState('Avenida de Carlos II 2 Córdoba');
 
   return (
+
     <div className="App">
+      <h1>Buscador</h1>
+
       <button type="button" onClick={() => setDireccion('Plaza de la Corredera Córdoba')}>
         Cambiar direccion
       </button>
       <ul className="listing">
         <ListOfSingleGeocode direccion={direccion} />
       </ul>
+
     </div>
 
   );
